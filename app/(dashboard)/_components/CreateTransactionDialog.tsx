96fbd0983adb25430de8c43824bac794dc2235db
Atharv Vani <atharvavani110@gmail.com>
2024-07-01 00:52:14 +0530

    add: transaction form & dialog with initialization of forms

---
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { ReactNode } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  CreateTransctionSchema,
+  CreateTransctionSchemaType,
+} from "@/schema/transaction";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface CreateTransactionDialogProps {
+  trigger: ReactNode;
+  type: TransactionType;
+}
+
+const CreateTransactionDialog = ({
+  trigger,
+  type,
+}: CreateTransactionDialogProps) => {
+  const methods = useForm<CreateTransctionSchemaType>({
+    resolver: zodResolver(CreateTransctionSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  });
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="tracking-wide">
+            Create a new{" "}
+            <span
+              className={
+                type === "income" ? "text-emerald-500" : "text-rose-500"
+              }
+            >
+              {type}
+            </span>{" "}
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...methods}>
+          <form className="space-y-4">
+            <FormField
+              control={methods.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue="" {...field} />
+                  </FormControl>
+                  <FormDescription className="text-xs">
+                    Transaction description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={methods.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={0} type="number" {...field} />
+                  </FormControl>
+                  <FormDescription className="text-xs">
+                    Transaction amount (required)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between gap-2"></div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateTransactionDialog;
